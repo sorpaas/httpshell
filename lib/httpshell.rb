302ad4b508d353cd3dc0105173065b9a3ac9e2ec
sorpa'as plat <sorpaas@gmail.com>
2014-05-05 18:05:27 +0800

    solve working path not current issue

--- lib/httpshell.rb
+++ lib/httpshell.rb
@@ -20,17 +20,15 @@ module HTTPShell
     
     private
     def cd_relatively(path)
-      if not /^(http|https|file):\/\/$/i =~ path
-        path = path.sub /^\/+/, ''
-        path = path.sub /\/+$/, ''
-      end
+      path = path.sub /^\/+/, ''
+      path = path.sub /\/+$/, ''
       
       if path == '..'
         @working_path = @working_path.sub /\/[^\/]+$/, ''
       elsif path == '.'
         @working_path = @working_path
       else
-        if /^(http|https|file):\/\/$/i =~ path
+        if /^(http|https|file):\/\/$/i =~ @working_path
           @working_path = "#{@working_path}#{path}"
         else
           @working_path = "#{@working_path}/#{path}"
