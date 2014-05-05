bc8335ac3be70d93d04204e125241c394a7adb23
sorpa'as plat <sorpaas@gmail.com>
2014-05-05 17:54:17 +0800

    fix cd slashy bug

--- lib/httpshell.rb
+++ lib/httpshell.rb
@@ -20,15 +20,21 @@ module HTTPShell
     
     private
     def cd_relatively(path)
-      path = path.sub /^\//, ''
-      path = path.sub /\/$/, ''
+      if not /^(http|https|file):\/\/$/i =~ path
+        path = path.sub /^\/+/, ''
+        path = path.sub /\/+$/, ''
+      end
       
       if path == '..'
         @working_path = @working_path.sub /\/[^\/]+$/, ''
       elsif path == '.'
         @working_path = @working_path
       else
-        @working_path = "#{@working_path}/#{path}"
+        if /^(http|https|file):\/\/$/i =~ path
+          @working_path = "#{@working_path}#{path}"
+        else
+          @working_path = "#{@working_path}/#{path}"
+        end
       end
     end
     
